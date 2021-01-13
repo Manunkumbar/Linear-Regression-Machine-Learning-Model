67acf5dfeb1c9bdcc81331f8d92f3df6fb05c582
Manunkumbar <49186602+Manunkumbar@users.noreply.github.com>
2021-01-13 18:51:09 +0530

    initial commit

---
+++ model.py
@@ -0,0 +1,49 @@
+
+import numpy as np
+import pandas as pd
+
+# print(sys.argv)
+
+import pickle
+'''
+housing_median_age
+total_rooms
+total_bedrooms
+population
+households
+median_income
+ocean_proximity
+'''
+
+'''
+args = sys.argv
+if len(args)!=8:
+    print("Usage :")
+    print(" python main.py <housing_median_age> <total_rooms> <total_bedrooms> <population> <households> <median_income> <ocean_proximity>")
+    exit()
+
+housing_median_age = float(args[1])
+total_rooms        = float(args[2])
+total_bedrooms     = float(args[3])
+population         = float(args[4])
+households         = float(args[5])
+median_income      = float(args[6])
+ocean_proximity    = float(args[7])
+'''
+housing_median_age = float(input("Enter housing_median_age : "))
+total_rooms = float(input("Enter total_rooms : "))
+total_bedrooms = float(input("Enter total_bedrooms : "))
+population = float(input("Enter population : "))
+households = float(input("Enter households : "))
+median_income = float(input("Enter median_income : "))
+ocean_proximity = float(input("Enter ocean_proximity :"))
+
+X = np.array([[housing_median_age,total_rooms,total_bedrooms,population,households,median_income,ocean_proximity]])
+
+# load model from saved file
+f = open("LR_Model.sav","rb")
+model = pickle.load(f)
+f.close()
+median_house_price = model.predict(X)[0]
+
+print("median house price for your home is $",median_house_price)
